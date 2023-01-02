522160db81a55146794755e22703463fbb9f65fb
Maks1232 <maksymilian.karasek@o2.pl>
2023-01-02 17:21:33 +0100

    tworzenie many to many przez through

--- app/models/groupinfo.rb
+++ app/models/groupinfo.rb
@@ -1,3 +1,6 @@
 class Groupinfo < ApplicationRecord
-  has_and_belongs_to_many(:users)
+  #has_and_belongs_to_many(:users)
+
+  has_many :groupmembers
+  has_many :users, through: :groupmembers
 end
